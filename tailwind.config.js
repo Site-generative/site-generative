01389a47461fd17b572a7555dceb60ead75c6533
Tomáš Toman <97306489+tomas-toman@users.noreply.github.com>
2024-04-23 13:03:10 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './.html',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
+  jit: true,
 }
 
