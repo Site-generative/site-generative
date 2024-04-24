8ee24031b0c016a6d304e2729b3ccf3e196fd697
Tomáš Toman <tomastoman52@email.cz>
2024-04-24 16:29:37 +0200

    E-mail fix + footer update

--- src/email_sender.php
+++ src/email_sender.php
@@ -1,29 +1,22 @@
 <?php
-    ini_set( 'display_errors', 1 );
-    error_reporting( E_ALL );
+    ini_set( 'display_errors', 'Off' );
+    error_reporting( 0 );
     $to = 'info@site-generative.com';
-    // Sanitize and validate input
     $contact = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-    $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
+    $message = $_POST['message'];
 
     if ($contact && $message) {
-        // Set up the email parameters
         $subject = 'New message from contact form';
         $headers = 'From: ' . $contact . "\r\n" .
                    'Reply-To: ' . $contact . "\r\n" .
                    'X-Mailer: PHP/' . phpversion();
 
-        echo 'Email: ' . $contact . '<br>' .
-             'Message: ' . $message;
-        // Send the email
         if (mail($to, $subject, $message, $headers)) {
-            echo 'Email sent successfully.';
+            echo '<svg xmlns="http://www.w3.org/2000/svg" height="22px" width="22px" viewBox="0 0 512 512" class="mr-2"><path fill="#22c55e" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/></svg><p class="text-green-500">E-mail byl úspěšně odeslán, brzy se vám ozveme.</p>';
         } else {
-            echo 'Failed to send email.';
+            echo '<svg xmlns="http://www.w3.org/2000/svg" height="22px" width="22px" viewBox="0 0 512 512" class="mr-2"><path fill="#ef4444" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24V264c0 13.3-10.7 24-24 24s-24-10.7-24-24V152c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/></svg><p class="text-red-500">Nepodařilo se odeslat e-mail.</p>';
         }
     } else {
-        echo 'Invalid input.';
+        echo '<svg xmlns="http://www.w3.org/2000/svg" height="22px" width="22px" viewBox="0 0 512 512" class="mr-2"><path fill="#ef4444" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24V264c0 13.3-10.7 24-24 24s-24-10.7-24-24V152c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/></svg><p class="text-red-500">Neplatný e-mail nebo zpráva.</p>';
     }
-    //echo '<br><a href="index.html">Back to contact form</a>';
-    header('Location: ../index.html');
 ?>
